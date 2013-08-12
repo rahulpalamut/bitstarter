d60e7565be487a1830f6fd95420e7a1d8b55518b
Rahul <rahulpalamut@yahoo.com>
2013-08-12 05:01:42 +0000

    Updated web.js: file buffer I/O

--- web.js
+++ web.js
@@ -1,9 +1,10 @@
-var express = require('express');
+ var express = require('express');
 var app = express();
 app.use(express.logger());
+var buf = new Buffer(fs.readFileSync('./index.html'))
 
 app.get('/', function(request, response) {
-  response.send('Hello World2!');
+  response.send(buf.toString());
 });
 
 var port = process.env.PORT || 5000;
